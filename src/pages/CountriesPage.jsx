a5eeda7efcc1c70da705456feb990ae9baeced8f
JustinaAl <juste.paskeviciute@gmail.com>
2025-09-16 15:57:02 +0200

    display countries and paging

--- src/pages/CountriesPage.jsx
+++ src/pages/CountriesPage.jsx
@@ -1,15 +1,57 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SelectDropdown from "../components/SelectDropdown";
-import { setRegion } from "../redux/regionSlice";
+import { fetchCountries, setPage, setRegion } from "../redux/regionSlice";
+import { useEffect } from "react";
+
+const itemsPerPage = 12;
 
 const CountriesPage = () => {
 
-    const {regions, selectedRegion} = useSelector(store => store.regions);
+    const {regions, selectedRegion, countries, status, error, currentPage} = useSelector(store => store.regions);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(fetchCountries(selectedRegion));
+    }, [selectedRegion]);
+
+
+    const countiesToDisplay = countries
+    .slice(itemsPerPage * currentPage, itemsPerPage * (currentPage + 1))
+    .map((country, index) => <div 
+    key={'c'+index}
+    className="border border-black aspect-[2/1.3] hover:scale-95 cursor-pointer"
+    ><img
+    className="h-[100%] w-full object-cover"
+    src={country.flags.svg} alt={country.flags.alt}></img></div>)
+
+    const numberOfPages = Math.ceil(countries.length / itemsPerPage)
+    const buttonsForPages = []
+
+    for(let i = 0; i<numberOfPages; i++){
+        buttonsForPages.push(
+        <button
+            className="p-2 w-12 hover:scale-110"
+            key={`pg${i}`}
+            onClick={()=> dispatch(setPage(i))}
+                >{i + 1}
+        </button>)
+    }
+    
+    
 
     return(
-        <div>
-            <h1 className="text-4xl">Select Region</h1>
+        <div className="md:p-4 lg:p-6 h-full flex flex-col justify-center">
+            <h1 className="text-4xl md:text-5xl mb-4">Select Region</h1>
             <SelectDropdown options={regions} value={selectedRegion} setValue={setRegion}/>
+            <div>
+                <div className="grid grid-cols-3 gap-4 md:gap-8 py-12">
+                    {countiesToDisplay}
+                </div>
+                <div className="flex justify-center gap-4 ">
+                    {buttonsForPages}
+                </div>
+            </div>
         </div>
     )
 }
--- src/redux/regionSlice.js
+++ src/redux/regionSlice.js
@@ -1,16 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchCountries = createAsyncThunk(
+    'countries/fetchCountries',
+    async (region) => {
+        const response = await fetch(`https://restcountries.com/v3.1/region/${region}?fields=name,flags,languages,currencies,population,capital`);
+        return await response.json();
+    }
+)
 
 const regionSlice = createSlice({
     name: 'regionSlice',
     initialState:{
         regions: ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'],
         selectedRegion: 'Africa',
+
+        countries: [],
+        status: 'idle',
+        error: null,
+
+        currentPage: 0,
     },
     reducers:{
-        setRegion: (state, action) => {state.selectedRegion = action.payload} 
-    }
-})
+        setRegion: (state, action) => {state.selectedRegion = action.payload},
+        setPage: (state, action) => {state.currentPage = action.payload}
+    },
+
+    extraReducers: (builder) =>{
+        builder
+        .addCase(fetchCountries.fulfilled, (state, action) => {
+            state.status = 'Success!';
+            console.log(action.payload);
+            state.countries = action.payload;
+        })
+        .addCase(fetchCountries.pending, (state) => {
+            state.status = 'Loading';
+        })
+        .addCase(fetchCountries.rejected, (state, action) => {
+            state.status = 'Failed';
+            state.error = action.error.message
+        });
+    },
+});
 
 
-export const {setRegion} = regionSlice.actions
+export const {setRegion,setPage} = regionSlice.actions
 export default regionSlice.reducer
